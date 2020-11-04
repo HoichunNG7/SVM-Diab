b769cf6c93831c03310fc2909e1460394d6f6aa3
Hoichun Ng <hoichunng99@163.com>
2020-11-04 18:12:46 +0800

    use sklearn svm tools for comparison

---
+++ src/sklearn_svm.py
@@ -0,0 +1,67 @@
+from svm.classifiers.linear_svm import linear_svm_loss_vectorized
+import time
+import pandas as pd
+from sklearn import svm
+from sklearn.datasets import load_diabetes
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+diabetes = pd.read_csv('svm/datasets/diabetes.csv')
+x_train, x_test, y_train, y_test = train_test_split(np.array(diabetes.loc[:, diabetes.columns != 'Outcome']), np.array(diabetes['Outcome']), stratify=np.array(diabetes['Outcome']), random_state=66)
+
+num_training = 480
+num_validation = 96 
+num_test = 160
+
+# Change label set from {0, 1} to {-1, 1}
+y_train = 2 * y_train - 1
+y_test = 2 * y_test - 1
+
+# Our validation set will be num_validation points from the original
+# training set.
+mask = range(num_training, num_training + num_validation)
+X_val = x_train[mask]
+y_val = y_train[mask]
+
+# Our training set will be the first num_train points from the original
+# training set.
+mask = range(num_training)
+X_train = x_train[mask]
+y_train = y_train[mask]
+
+# We use the first num_test points of the original test set as our
+# test set.
+mask = range(num_test)
+X_test = x_test[mask]
+y_test = y_test[mask]
+
+# invoke sklearn tools
+linear_svc = svm.SVC(kernel='linear')
+rbf_svc = svm.SVC(kernel='rbf')
+
+# Training
+tic0 = time.time()
+linear_svc.fit(X_train, y_train)
+toc0 = time.time()
+print('Linear svm training took %fs' % (toc0 - tic0))
+
+tic1 = time.time()
+rbf_svc.fit(X_train, y_train)
+toc1 = time.time()
+print('RBF kernel svm training took %fs' % (toc1 - tic1))
+print('\n')
+
+# Evaluation
+y_train_pred = linear_svc.predict(X_train)
+print('training accuracy: %f' % (np.mean(y_train == y_train_pred), ))
+y_val_pred = linear_svc.predict(X_val)
+print('validation accuracy: %f' % (np.mean(y_val == y_val_pred), ))
+
+y_train_pred = rbf_svc.predict(X_train)
+print('training accuracy: %f' % (np.mean(y_train == y_train_pred), ))
+y_val_pred = rbf_svc.predict(X_val)
+print('validation accuracy: %f' % (np.mean(y_val == y_val_pred), ))
+print('\n')
